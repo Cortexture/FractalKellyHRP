61e8f6f83b3af00575001e1be1f69ff505abd54c
Cortexture <marioacabada@gmail.com>
2024-01-12 11:48:43 -0600

    just made it into main until i pad up the project with it's accessory files

--- src/main.jl
+++ src/main.jl
@@ -1,3 +1,28 @@
+using CSV, DataFrames, DataFramesMeta
 
-println("Hello World")
+df_amd = CSV.read("../data/AMD.csv", DataFrame;
+    normalizenames  = true,
+    ignoreemptyrows = true,
+    types = String)
 
+#=
+df_coke = CSV.read("../data/COKE.csv", DataFrame;
+    normalizenames  = true,
+    ignoreemptyrows = true,
+    types = String)
+
+df_nvda = CSV.read("../data/NVDA.csv", DataFrame;
+    normalizenames  = true,
+    ignoreemptyrows = true,
+    types = String)
+
+df_tsla = CSV.read("../data/TSLA.csv", DataFrame;
+    normalizenames  = true,
+    ignoreemptyrows = true,
+    types = String)
+
+df_wmt = CSV.read("../data/WMT.csv", DataFrame;
+    normalizenames  = true,
+    ignoreemptyrows = true,
+    types = String)
+=#
