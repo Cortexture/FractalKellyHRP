47958fa95ff0cd39f62f6e5a359be37894e48082
Cortexture <marioacabada@gmail.com>
2024-02-24 22:30:14 -0600

    added a hardcoded implementation of the innerjoins, will work on better implementation later

--- src/main.jl
+++ src/main.jl
@@ -5,91 +5,28 @@ function load_dataframe()
     cd("../data/")
     names = @pipe readdir() .|> chop(_, tail = 4) .|> lowercase
     files = readdir(join = true)
-    dataframe_carrier = Vector{DataFrame}()
+    stocks = Vector{DataFrame}()
     for i in eachindex(files)
-        df_stock = CSV.read(files[i], DataFrame;
+        stock = CSV.read(files[i], DataFrame;
             normalizenames  = true,
             ignoreemptyrows = true,
             types = String)
-        @select!(df_stock, :Date, :Open, :Close)
-        @transform!(df_stock, 
+        @select!(stock, :Date, :Open, :Close)
+        @transform!(stock, 
             :Date = parse.(Date, :Date),
             :Open = parse.(Float64, :Open),
             :Close = parse.(Float64, :Close))
-        @subset!(df_stock, :Open .> 0)
-        @select!(df_stock, :Date, :Return = :Close ./ :Open)
+        @subset!(stock, :Open .> 0)
+        @select!(stock, :Date, :Return = :Close ./ :Open)
         name = names[i]
-        rename!(df_stock, :Date => "date", :Return => "return_$name")
-        push!(dataframe_carrier, df_stock)
+        rename!(stock, :Date => "date", :Return => "return_$name")
+        push!(stocks, stock)
     end
-    return dataframe_carrier
+
+    # Hardcoded this, need a better implementation
+    stocks_df = innerjoin(stocks[1], stocks[2], stocks[3], stocks[4], stocks[5];
+        on = :date, makeunique = true)
+
 end
 
 test = load_dataframe()
-
-#=
-df_amd = CSV.read("../data/AMD.csv", DataFrame;
-    normalizenames  = true,
-    ignoreemptyrows = true,
-    types = String)
-
-@select!(df_amd, :Date, :Open, :Close)
-@transform!(df_amd, 
-    :Date = parse.(Date, :Date),
-    :Open = parse.(Float64, :Open),
-    :Close = parse.(Float64, :Close))
-@subset!(df_amd, :Open .> 0)
-@select!(df_amd, :Date, :return_amd = :Close ./ :Open)
-
-df_coke = CSV.read("../data/COKE.csv", DataFrame;
-    normalizenames  = true,
-    ignoreemptyrows = true,
-    types = String)
-
-@select!(df_coke, :Date, :Open, :Close)
-@transform!(df_coke, 
-    :Date = parse.(Date, :Date),
-    :Open = parse.(Float64, :Open),
-    :Close = parse.(Float64, :Close))
-@subset!(df_coke, :Open .> 0)
-@select!(df_coke, :Date, :return_coke = :Close ./ :Open)
-
-df_nvda = CSV.read("../data/NVDA.csv", DataFrame;
-    normalizenames  = true,
-    ignoreemptyrows = true,
-    types = String)
-
-@select!(df_nvda, :Date, :Open, :Close)
-@transform!(df_nvda, 
-    :Date = parse.(Date, :Date),
-    :Open = parse.(Float64, :Open),
-    :Close = parse.(Float64, :Close))
-@subset!(df_nvda, :Open .> 0)
-@select!(df_nvda, :Date, :return_nvda = :Close ./ :Open)
-
-df_tsla = CSV.read("../data/TSLA.csv", DataFrame;
-    normalizenames  = true,
-    ignoreemptyrows = true,
-    types = String)
-
-@select!(df_tsla, :Date, :Open, :Close)
-@transform!(df_tsla, 
-    :Date = parse.(Date, :Date),
-    :Open = parse.(Float64, :Open),
-    :Close = parse.(Float64, :Close))
-@subset!(df_tsla, :Open .> 0)
-@select!(df_tsla, :Date, :return_tsla = :Close ./ :Open)
-
-df_wmt = CSV.read("../data/WMT.csv", DataFrame;
-    normalizenames  = true,
-    ignoreemptyrows = true,
-    types = String)
-
-@select!(df_wmt, :Date, :Open, :Close)
-@transform!(df_wmt, 
-    :Date = parse.(Date, :Date),
-    :Open = parse.(Float64, :Open),
-    :Close = parse.(Float64, :Close))
-@subset!(df_wmt, :Open .> 0)
-@select!(df_wmt, :Date, :return_wmt = :Close ./ :Open)
-=#
