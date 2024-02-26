4c775117fc9f024a0ef7fe23579452f086dc8711
Cortexture <marioacabada@gmail.com>
2024-02-25 18:07:38 -0600

    think i am done for the day

--- src/main.jl
+++ src/main.jl
@@ -1,6 +1,7 @@
 using Pipe: @pipe
 using CSV, DataFrames, DataFramesMeta, Dates
 using HypothesisTests
+using StatsBase
 
 function load_dataframe()
     cd("../data/")
@@ -31,8 +32,48 @@ end
 returns_df = load_dataframe()
 
 amd = returns_df.return_amd
-blah = ADFTest(amd, :none, 0)
+
+# now need to get the fractional differences
+# amd[2]*fraction - amd[1] and so on and so on
+
+function find_fraction(vec)::Float64
+    for i ∈ 1:-0.01:0
+        diff_vec = Vector{Float64}()
+        push!(diff_vec, 0)
+        for j ∈ 2:length(vec)
+            push!(diff_vec, vec[j]*i - vec[j-1])
+        end
+        test_statistics = ADFTest(diff_vec, :none, 1)
+        if test_statistics.stat < test_statistics.cv[2]
+            continue
+        else test_statistics.stat > test_statistics.cv[2]
+            return i+0.01
+        end
+    end
+end
+
+fraction = find_fraction(amd)
+
+function get_differences(vec, frac)
+    diff_vec = Vector{Float64}()
+    push!(diff_vec, 0.0)
+    for i ∈ 2:length(vec)
+        push!(diff_vec, vec[i]*frac - vec[i-1])
+    end
+    return diff_vec
+end
+
+println(first(amd, 10))
+println(mean(amd))
+
+diff_amd = get_differences(amd, fraction) 
+stationary_amd = diff_amd .- mean(diff_amd)
+
+println(first(stationary_amd, 10))
+println(mean(stationary_amd))
+
+#=blah = ADFTest(amd, :none, 0)
 println(blah.stat)
 println(blah.cv[2])
 println(blah.stat < blah.cv[2])
-
+=#
